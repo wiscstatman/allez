46c21e8fcd3f0d81826aa609aace5b762dfc4ce7
manewton <wiscstatman@gmail.com>
2025-07-01 22:29:28 -0500

    order of y axis labels

--- R/allezPlot.R
+++ R/allezPlot.R
@@ -121,9 +121,10 @@ allezplot <- function(aOrd, allez.out,
         fill = score
         )
       ) + 
-    scale_y_discrete(limits = colnames(aOrd), position="left") + 
+#    scale_y_discrete(limits = colnames(aOrd), position="left") + 
+    scale_y_discrete(limits = rev(colnames(aOrd)), position="left") + 
     theme( legend.position="none", base_size=11 ) +
-    annotate(geom="text", x = xpos + 2, y = (1:nsets) - (1/2), label = term$term, hjust = 0, size=11/(2*.pt) )+ 
+    annotate(geom="text", x = xpos + 2, y = (nsets:1) - (1/2), label = term$term, hjust = 0, size=11/(2*.pt) )+ 
 	coord_cartesian(clip = 'off') +
     scale_color_gradient(low = "#FFFFFF00", high = "#000000FF")+ 
     scale_fill_gradient(low = "#FFFFFF00", high = "#000000FF") +
